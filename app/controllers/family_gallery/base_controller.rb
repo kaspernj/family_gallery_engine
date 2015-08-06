264e63521cd3b806fd02403d4ef9e7c94053b1ff
kaspernj <k@spernj.org>
2015-08-06 22:05:47 +0200

    Removed debug output

--- app/controllers/family_gallery/base_controller.rb
+++ app/controllers/family_gallery/base_controller.rb
@@ -6,15 +6,8 @@ class FamilyGallery::BaseController < ActionController::Base
   include LightMobile::DynamicRenderer
 
   before_filter :set_locale
-  before_filter :debug_output
 
 private
-
-  def debug_output
-    logger.warn "AgentMobile: #{view_context.agent_mobile?}"
-    logger.warn "AgentBrowser: #{view_context.agent_browser}"
-    logger.warn "UserAgent: #{view_context.request.user_agent}"
-  end
 
   def set_locale
     session[:locale] ||= I18n.default_locale
