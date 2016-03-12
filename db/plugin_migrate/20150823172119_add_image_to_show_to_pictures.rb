471255098d2efb2c1e0066600653073d5135d00c
kaspernj <k@spernj.org>
2016-03-12 15:29:31 +0100

    Only add attachment if it doesn't already exist

--- db/plugin_migrate/20150823172119_add_image_to_show_to_pictures.rb
+++ db/plugin_migrate/20150823172119_add_image_to_show_to_pictures.rb
@@ -1,5 +1,9 @@
 class AddImageToShowToPictures < ActiveRecord::Migration
-  def change
-    add_attachment :family_gallery_pictures, :image_to_show
+  def up
+    add_attachment :family_gallery_pictures, :image_to_show unless column_exists? :family_gallery_pictures, :image_to_show_file_name
+  end
+
+  def down
+    remove_attachment :family_gallery_pictures, :image_to_show
   end
 end
