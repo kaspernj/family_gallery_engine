1b6f7ed9bf60074200f3558139c51aba641f5a2d
kaspernj <k@spernj.org>
2016-03-12 15:21:47 +0100

    Tons of UI and minor fixes

--- app/models/family_gallery/user_role.rb
+++ app/models/family_gallery/user_role.rb
@@ -4,9 +4,17 @@ class FamilyGallery::UserRole < ActiveRecord::Base
   validates :role, uniqueness: {scope: :user_id}
   validates :role, :user, presence: true
 
-  def self.role_options
+  def self.translated_roles
     {
       t(".administrator") => "administrator"
     }
+  end
+
+  def translated_role
+    FamilyGallery::UserRole.translated_roles.each do |translation, role_i|
+      return translation if role_i == role
+    end
+
+    raise "No such role: #{role}"
   end
 end
--- lib/family_gallery/engine.rb
+++ lib/family_gallery/engine.rb
@@ -1,3 +1,5 @@
+module FamilyGallery; end
+
 class FamilyGallery::Engine < ::Rails::Engine
   isolate_namespace FamilyGallery
 end
